3129a866c9031f31df13ef4077cd8c04a27f1969
Marius Guščius <marius.guscius@mailbox.org>
2024-01-01 14:00:30 +0200

    typo

--- __loader.sh
+++ __loader.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-functions_dir="$(dirname ${BASH_SOURCE})"
+functions_dir="$(dirname "${BASH_SOURCE}")"
 for file in $(find "${functions_dir}" -iname '*.sh' ! -iname '__loader.sh'); do
-	function_dir="$(dirname ${file##"${functions_dir}/"})"
+	function_dir="$(dirname "${file##"${functions_dir}/"}")"
 	if [[ "${function_dir}" != "." ]]; then
 		function_namespace="${function_dir//\//.}."
 	else
 		function_namespace=""
 	fi
-	file_name="$(basename ${file})"
+	file_name="$(basename "${file}")"
 	function_name="${file_name%.sh*}"
-	function_body="$(cat ${file})"
+	function_body="$(cat "${file}")"
 	eval "$(echo -e "${function_namespace}${function_name}() {\n${function_body}\n}")"
 done
